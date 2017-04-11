7f06f8aa22ff6f095718ba922d531caab50361e3
Tom Gao <tomgaoyuan@yahoo.com>
2017-04-11 11:30:02 +0800

    Create a new branch

--- SyntaxLearning/src/InCoding/ExceptionTest.java
+++
@@ -1,9 +0,0 @@
-package InCoding;
-
-public class ExceptionTest {
-	public static void  throwTest() throws IException{	
-	//ok like this
-	//public static void  throwTest(){
-		throw new IException("IException");
-	}
-}
